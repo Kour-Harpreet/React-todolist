ed50fa3c72fa5a247f0994552f432eef75f9cf3c
harpreet <Harpreet Kour>
2020-07-27 11:58:29 -0600

    Add logic for 'add new todo'; add spread operator example.

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends React.Component {
    constructor ( props )
@@ -11,10 +12,29 @@ class App extends React.Component {
     };
   }
 
-  addToDo ( event )
+  // Add a new todo (see onSubmit in our form below.)
+  addToDo = ( event ) =>
   {
     event.preventDefault(); // Stop the page from reloading.
-    console.log( "Test add todo!" );
+    // console.log( "Test add todo!" ); // Test that we're submitting!
+
+    // Set up new task.
+    const newTask = {
+      uniqueId: uuidv4(), // Ensure a unique ID.
+      value: this.state.newToDo // Read current "new todo" value.
+    };
+
+    console.log( newTask ); // Check to see if newTask is generating okay.
+
+    // Create a clone of our ToDos array, so we can make changes before updating state.
+    const currentToDoList = [...this.state.toDos]; // "..." is the spread operator.
+    currentToDoList.push( newTask ); // Add our new task to the clone array.
+
+    // Use "setState" to update any state data (never re-assign directly!)
+    this.setState( { // This is why we made a clone of the to-do list, and updated it before running setState again.
+      toDos: currentToDoList, // Update todos list.
+      newToDo: "" // Clear the "new to-do" field.
+    } );
   }
   updateItem ( key, value )
   {
@@ -31,7 +51,7 @@ class App extends React.Component {
       <h1>React To-Do App</h1>
       <form onSubmit={this.addToDo}>
         <label htmlFor="newToDo">
-          Enter a new "To-Do:"
+        Enter a new "To-Do":
           <input
               type="text"
               name="newToDo"
