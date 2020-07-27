3b971b2815750b2992160bff6370456d4741740a
harpreet <Harpreet Kour>
2020-07-27 10:44:42 -0600

    add on submit event for the form; prevent default form submission function

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 class App extends React.Component {
+   constructor ( props )
+  {
+    super( props );
+
+    this.state = {
+      newToDo: "", // Keep track of our new to-do value.
+      toDos: [] // Keep track of all the todos.
+    };
+  }
+
+  addToDo ( event )
+  {
+    event.preventDefault();
+    console.log( "Test add todo!" );
+  }
   render() {
     return (
   
       <>
       <h1>React To-Do App</h1>
-      <form>
+      <form onSubmit={this.addToDo}>
         <label htmlFor="newToDo">
           Enter a new "To-Do:"
           <input type="text" name="newToDo" id="newToDo" required />
