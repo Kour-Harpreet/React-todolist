962b3f7f89e11b9dd889b052d9e1a0d4f8015d85
harpreet <Harpreet Kour>
2020-07-27 10:35:07 -0600

    removed unnecessary code, add new to-do form JSX to app

---
+++ to-do-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+  
+      <>
+      <h1>React To-Do App</h1>
+      <form>
+        <label htmlFor="newToDo">
+          Enter a new "To-Do:"
+          <input type="text" name="newToDo" id="newToDo" required />
+        </label>
+        <input type="submit" value="Add New To-Do" />
+      </form>
+      <h2>Current To-Dos:</h2>
+      <ul>
+
+      </ul>
+    </>
+    );
+  }
+
+}
+
+export default App;
